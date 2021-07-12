d8063f3c1f8ae6b5036ef91a1518e227c96d146c
abiu <abiuhi@qq.com>
2021-07-12 16:14:52 +0800

    ++

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 let cp = require('child_process');
-const sp = cp.spawn('/usr/local/bin/node',['./index.js'],{
+const sp = cp.spawn('/Users/abiu/.nvm/versions/node/v11.10.0/bin/node',['./index.js'],{
     detached: true,
 });
 
